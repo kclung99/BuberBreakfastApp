8453fb4a677287dc7912263a3a555a64ae268ce2
KC Lung <kclung99@gmail.com>
2022-11-30 16:18:23 +0800

    Finish CreateBreakfast end point

---
+++ BuberBreakfast.Contract/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberBreakfast.Contract.Breakfast
+{
+    public record BreakfastResponse(
+        Guid Id,
+        string Name,
+        string Description,
+        DateTime StartDateTime,
+        DateTime EndDateTime,
+        DateTime LastModifiedDateTime,
+        List<string> Savory,
+        List<string> Sweet);
+}
---
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,63 @@
+﻿using BuberBreakfast.Contract.Breakfast;
+using BuberBreakfast.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers
+{
+    [ApiController]
+    [Route("breakfasts")]
+    public class BreakfastsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+        {
+            // Map request to the internal service model (contract can be changed without changing the internal model)
+            var breakfast = new Breakfast(
+                Guid.NewGuid(),
+                request.Name,
+                request.Description,
+                request.StartDateTime,
+                request.EndDateTime,
+                DateTime.UtcNow,
+                request.Savory,
+                request.Sweet);
+
+            // TODO: save to db
+
+            // Map result to response contract
+            var response = new BreakfastResponse(
+                breakfast.Id,
+                breakfast.Name,
+                breakfast.Description,
+                breakfast.StartDateTime,
+                breakfast.EndDateTime,
+                breakfast.LastModifiedDateTime,
+                breakfast.Savory,
+                breakfast.Sweet);
+
+            // Return created action
+            return CreatedAtAction(
+                actionName: nameof(GetBreakfast),
+                routeValues:  new {id = response.Id},
+                value: response);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpsertBreakfast(Guid id, CreateBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult UpsertBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}
